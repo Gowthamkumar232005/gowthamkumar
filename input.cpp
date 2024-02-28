0e8947cb712d7392172219ac648d95ed05f05a6f
Gowthamkumar232005 <160147267+Gowthamkumar232005@users.noreply.github.com>
2024-02-28 13:35:17 +0530

    Create input.cpp

---
+++ input.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    float f;
+    double d;
+    char ch,str[10];
+    cout<<"Enter an integer : ";
+    cin>>n;
+    cout<<"Enter an flaoting point : ";
+    cin>>f;
+    cout<<"Enter a double floating point : ";
+    cin>>d;
+    cout<<"Enter a character : ";
+    cin>>ch;
+    cout<<"Enter a string : ";
+    cin>>str;
+}
